befac2ccfa482b91aa8589424da94a61ee242525
Victor M <virtus.mv@gmail.com>
2023-05-28 11:42:52 +0700

    add bposts app (blog-like features)

--- mysite/bposts/urls.py
+++ mysite/bposts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from bposts.views import PostsListView, PostsDetailView
 
+app_name = 'bposts'
 urlpatterns = [
     path('posts/', PostsListView.as_view(), name='posts_list'),
-    path('posts/<int:pk>/', PostsDetailView.as_view(), name='posts_list'),
+    path('posts/<int:pk>/', PostsDetailView.as_view(), name='posts_detail'),
 ]
