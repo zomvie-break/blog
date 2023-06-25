79391a6d7b69fa7b777e9166a67fc69a5b4e5a65
Victor M <virtus.mv@gmail.com>
2023-06-25 16:02:14 +0700

    add STATIC_ROOT to settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -154,6 +154,7 @@ class Dev(Configuration):
     STATICFILES_DIR = [
         os.path.join(BASE_DIR, 'static'),
     ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
 
     # Default primary key field type
     # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
