e7bdea89c926967ac3b184d551770ee5ebc14ae6
Victor M <virtus.mv@gmail.com>
2023-07-02 13:15:39 +0700

    modify homepage so that uses django orm

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Homepage(models.Model):
+    title = models.TextField(max_length=200)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Homepage
 
 # Create your views here.
 
 class MainView(View):
     template_name='home/main.html'
     def get(self, request):
-        title = 'This is my blog'
-        context = {'title':title}
-        return render(request, self.template_name, context)
+        homepage = Homepage.objects.get(id=1)
+        context = {'object':homepage}
+        return render(request, self.template_name, context) 
