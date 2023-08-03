602d9d95928935276e314502b697fb5216ed4b4b
Victor M <virtus.mv@gmail.com>
2023-08-03 16:46:31 +0700

    add projects to index.html

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Homepage
+from myprojects.models import Project
 
 # Create your views here.
 
 class MainView(View):
     template_name='home/index.html'
     def get(self, request):
-        homepage = Homepage.objects.get(id=1)
-        context = {'object':homepage}
+        projects = Project.objects.all()
+        context = {'projects':projects}
         return render(request, self.template_name, context) 
