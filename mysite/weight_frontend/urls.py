1bbf2ffd77211498544c3d6ba7bbda9aecef5b9a
Victor M. <virtus.mv@gmail.com>
2023-09-14 16:43:41 +0700

    add app_name to urls and to templates

--- mysite/weight_frontend/urls.py
+++ mysite/weight_frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "weight-frontend"
+
 urlpatterns = [
     path('sample/', views.WeightIndexView.as_view(), name='weight-sample'),
     path('user/', views.WeightUserView.as_view(), name='weight-user'),
